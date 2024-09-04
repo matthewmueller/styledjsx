2c510fd9a8715ae605b54d222968c914cfc2df54
Matthew Mueller <mattmuelle@gmail.com>
2024-09-03 23:44:48 -0500

    temp version

--- styledjsx_test.go
+++ styledjsx_test.go
@@ -211,3 +211,65 @@ export default function () {
 	`
 	diff.TestContent(t, actual, expected)
 }
+
+func TestTempl(t *testing.T) {
+	input := `
+templ Story(story *hackernews.Story) {
+  <div class="story">
+    <div>
+      <a>
+        { story.Title }
+      </a>
+      if story.URL  != "" {
+        <a class="url" href={ templ.URL(story.URL) }>({ formatURL(story.URL) })</a>
+      }
+    </div>
+    <div class="meta">
+      { strconv.Itoa(story.Points) } points by { story.Author } • { " " }
+      @Time(story.CreatedAt)
+    </div>
+    <style scoped>{` + "`" + `
+      .story {
+        background: red;
+      }
+      .url {
+        text-transform: none;
+      }
+      .meta {
+        padding: 10px;
+      }
+    ` + "`" + `}</style>
+  </div>
+}
+`
+
+	rewriter := styledjsx.New()
+	actual, err := rewriter.Rewrite("input.jsx", string(input))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := `
+    import Style from "styled-jsx";
+
+    templ Story(story *hackernews.Story) {
+      <div class="jsx-4GjC3K story">
+        <div class="jsx-4GjC3K">
+          <a class="jsx-4GjC3K">
+            { story.Title }
+          </a>
+          if story.URL  != "" {
+            <a class="jsx-4GjC3K url" href={ templ.URL(story.URL) }>({ formatURL(story.URL) })</a>
+          }
+        </div>
+        <div class="jsx-4GjC3K meta">
+          { strconv.Itoa(story.Points) } points by { story.Author } • { " " }
+          @Time(story.CreatedAt)
+        </div>
+        <Style scoped id="jsx-4GjC3K">{` + "`" + `.story.jsx-4GjC3K { background: red }
+    .url.jsx-4GjC3K { text-transform: none }
+    .meta.jsx-4GjC3K { padding: 10px }` + "`" + `}</Style>
+      </div>
+    }
+  `
+	diff.TestContent(t, actual, expected)
+}
